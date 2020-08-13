6e6944b1de10f25747179e75f85ceedf29f1a3d6
Tirupathi <tirupath@xilinx.com>
2020-08-13 14:12:01 +0000

    System controller: updated version details
    Modified path to sc_app to load from user bin folder.
    
    Signed-off-by: Tirupathi <tirupath@xilinx.com>

--- src/config_app.py
+++ src/config_app.py
@@ -1,14 +1,13 @@
 
 
 app_config = {
-    "deployment":"DEBUG"              # RELEASE, DEBUG
+    "deployment":"RELEASE"              # RELEASE, DEBUG
     ,"major_version":"α"
-    ,"minor_version" : "0"
+    ,"minor_version" : "2"
     ,"dev_for_major_ver" : "β"
     ,"dev_minor_ver" : "001"
-    ,"sc_app_path":"./sc_app"
-    ,"sensors_app_path":"test"
-    #,"sensors_app_path":"sensors"
+    ,"sc_app_path":"sc_app"
+    ,"sensors_app_path":"sensors"
     ,"config_sc_list_cmds":["listpower","listclock","listvoltage","listgpio"]
     ,"config_bit_list_cmds":["listBIT"]
     ,"config_bm_list_cmds":["listbootmode"]
