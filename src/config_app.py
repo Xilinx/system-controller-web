7c4d05b8667a1d59fb25f2984be7203bc9744254
Nagaradhesh Yeleswarapu <nagaradh@xilinx.com>
2021-01-22 15:37:56 +0530

    SystemCOntroller: update version number
    1. Update version number
    2. Delete temperory generated files
    
    Signed-off-by: Nagaradhesh Yeleswarapu <nagaradh@xilinx.com>

--- src/config_app.py
+++ src/config_app.py
@@ -8,10 +8,10 @@
 app_config = {
     "deployment":"RELEASE"              # RELEASE, DEBUG
     ,"major_version":"β"
-    ,"minor_version" : "0"
+    ,"minor_version" : "1"
     ,"dev_for_major_ver" : "β_1"
     ,"dev_minor_ver" : "008"
-    ,"sc_app_path":"./sc_app"
+    ,"sc_app_path":"sc_app"
     ,"config_sc_list_cmds":["listpower","listclock","listvoltage","listgpio","listSFP","listpowerdomain","listQSFP"]
     ,"config_bit_list_cmds":["listBIT"]
     ,"config_bm_list_cmds":["listbootmode"]
--- src/static/js/gen_bit.js
+++
@@ -1,8 +0,0 @@
-/*
-* Copyright (c) 2020, Xilinx Inc. and Contributors. All rights reserved.
-*
-* SPDX-License-Identifier: MIT
-*/
-var listsjson_bit = {
-"listBIT":["Check Clocks","Check Voltage"]
-}
