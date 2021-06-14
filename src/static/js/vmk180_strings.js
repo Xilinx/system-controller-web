e71ac5864f1ce395be92c0a0cb13d3e8c0ff08d3
Tirupathi <tirupath@xilinx.com>
2021-06-14 13:27:53 +0530

    SystemController: Remove PMTool for vmk180
    Signed-off-by: Tirupathi <tirupath@xilinx.com>

--- src/static/js/vmk180_strings.js
+++ src/static/js/vmk180_strings.js
@@ -82,7 +82,7 @@ var app_strings = {
 				"button_link":"https://www.xilinx.com/products/boards-and-kits/vmk180.html",
 				"learnmore_link":"https://xilinx-wiki.atlassian.net/wiki/spaces/A/pages/973013034/Versal+Prime+Series+VMK180+Evaluation+Kit",
 			},
-                        {
+                        /*{
                                 "title":"Power Management Dashboard",
                                 "image":"../static/images/xilinx_banner.png",
                                 "text":"",
@@ -90,7 +90,7 @@ var app_strings = {
                                 "button_link_type":1,
                                 "button_link": "launchpmtool",
                                 "learnmore_link":"",
-                        },
+                        },*/
 
 		],
 
