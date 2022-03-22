7ecab039bdd2da0d019d9c56e838375cb07d7440
Tirupathi <tirupath@xilinx.com>
2022-03-23 00:28:27 +0530

    update jupyter server start script
    Signed-off-by: Tirupathi <tirupath@xilinx.com>

--- scwebrun.sh
+++ scwebrun.sh
@@ -10,36 +10,7 @@ cd /usr/share/scweb/
 python3 systemcontroller.py >/dev/null 2>&1 &
 
 ## Run jupyter notebook with non root user
-cd /usr/share/example-notebooks/
-OWNER="petalinux"
-GROUP="petalinux"
-HOME=`(cd ~petalinux && pwd) || echo 'none'`
-NBDIR="${HOME}/notebooks"
-
-DAEMON_PATH="/sbin/start-jupyter.sh"
-DAEMON_NAME=`basename $DAEMON_PATH`
-PIDFILE="/var/run/${DAEMON_NAME}.pid"
-
-PATH=/bin:/usr/bin:/sbin:/usr/sbin
-
-. /etc/init.d/functions
-
-export JUPYTER_CONFIG_DIR="${HOME}/.jupyter"
-export JUPYTER_DATA_DIR="${HOME}/.local/share/jupyter"
-export JUPYTER_RUNTIME_DIR="${HOME}/.local/share/jupyter/runtime"
-export HOME="${HOME}"
-
-# check owner and group are valid
-id $OWNER > /dev/null 2>&1
-if [ "$?" = "1" ]; then echo "'$OWNER': no such owner... ERROR" ; exit 1 ; fi
-grep $GROUP /etc/group > /dev/null
-if [ "$?" = "1" ]; then echo "'$GROUP': no such group... ERROR" ; exit 1 ; fi
-
-# create nb dir if it doesn't exist
-if [ ! -d "$NBDIR" ] ; then install -o $OWNER -g $GROUP -d $NBDIR ; fi
-
-# start the daemon
-start-stop-daemon -S -c $OWNER:$GROUP -m -p $PIDFILE -x $DAEMON_NAME &
+sudo su - petalinux -c "systemctl --user enable jupyter-setup.service"
 
 ## print ip on console
 COUNT=25
