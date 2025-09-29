ba5530c9c74bc273a9777c4b170ebcec20e33a11
pranaykumar <pranay.kumar.chalamalla@amd.com>
2025-09-29 12:14:03 +0530

    Update the BEAM Startup script for Update8 release.
    Signed-off-by: pranaykumar <pranay.kumar.chalamalla@amd.com>

--- scwebrun.sh
+++ scwebrun.sh
@@ -46,11 +46,11 @@ fi
 
 ## print ip on console
 COUNT=30
-IP=`/sbin/ifconfig end0 | grep 'inet addr' | awk '{print $2}' | awk -F ':' '{print $2}'`
+IP=`/bin/ifconfig end0 | grep 'inet addr' | awk '{print $2}' | awk -F ':' '{print $2}'`
 while [ "$IP" == "" -a "$COUNT" != "0" ]; do
     sleep 1
     COUNT=`expr $COUNT - 1`
-    IP=`/sbin/ifconfig end0 | grep 'inet addr' | awk '{print $2}' | awk -F ':' '{print $2}'`
+    IP=`/bin/ifconfig end0 | grep 'inet addr' | awk '{print $2}' | awk -F ':' '{print $2}'`
 done
 
 echo | tee -a /dev/console
