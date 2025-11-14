7aae7ec6422ec896e81f85d57be93d7cef506719
pranaykumar <pranay.kumar.chalamalla@amd.com>
2025-11-14 16:01:43 +0530

    Update source for BEAM GUI.
    Signed-off-by: pranaykumar <pranay.kumar.chalamalla@amd.com>

--- src/restserv.py
+++ src/restserv.py
@@ -694,8 +694,8 @@ class ScriptRunner(Resource):
                 # Check if VRK160 OSPI boot file exists and device is VRK160
                 vrk160_boot_file = app_config["VRK160_ospirunscript"]
                 if os.path.exists(vrk160_boot_file) and deviname.strip() == "VRK160":
-                    result = SysFactory.exec_cmd(cmd, SysFactory.SCRIPT, app_config["ospirunstatusfile"] + " -b " + app_config["VRK160_ospirunscript"])
-                    print("VRK160 Ospi Flashing Result:", cmd, SysFactory.SCRIPT, app_config["ospirunstatusfile"] + " -b " + app_config["VRK160_ospirunscript"])
+                    result = SysFactory.exec_cmd(cmd + " -b " + app_config["VRK160_ospirunscript"], SysFactory.SCRIPT, app_config["ospirunstatusfile"])
+                    print("VRK160 Ospi Flashing Result:", cmd + " -b " + app_config["VRK160_ospirunscript"], SysFactory.SCRIPT, app_config["ospirunstatusfile"])
                 else:
                     result = SysFactory.exec_cmd(cmd, SysFactory.SCRIPT, app_config["ospirunstatusfile"])
                     print("Ospi Flashing Result:", cmd, SysFactory.SCRIPT, app_config["ospirunstatusfile"])
