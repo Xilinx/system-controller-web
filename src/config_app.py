71f8f49210e198a994e1181d6f60addf428ef698
pranaykumar <pranay.kumar.chalamalla@amd.com>
2026-02-02 11:11:37 +0530

    Update copyright year in source
    Signed-off-by: pranaykumar <pranay.kumar.chalamalla@amd.com>

--- src/config_app.py
+++ src/config_app.py
@@ -1,7 +1,7 @@
 
 ##
 # Copyright (c) 2020 - 2022 Xilinx, Inc.  All rights reserved.
-# Copyright (c) 2022 - 2025 Advanced Micro Devices, Inc.  All rights reserved.
+# Copyright (c) 2022 - 2026 Advanced Micro Devices, Inc.  All rights reserved.
 #
 # SPDX-License-Identifier: MIT
 ##
